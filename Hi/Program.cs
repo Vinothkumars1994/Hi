a5aa9a64a736f984133ed9c91451a27f4f0c6b9c
Vinoth <Vinothkumars90@gmail.com>
2020-01-04 11:11:32 +0530

    Add project files.

---
+++ Hi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!hiiiiiii");
+        }
+    }
+}
